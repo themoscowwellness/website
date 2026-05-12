6b9f9652eaf3d392c0f89dc7b80115ab75cc190a
themoscowwellness <themoscowwellness@gmail.com>
2026-05-12 12:05:02 +0000

    Initial Astro blog setup

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'The Moscow Wellness';
+export const SITE_DESCRIPTION =
+  'A curated guide to low-tox living, natural brands, and modern wellness culture in Moscow.';
